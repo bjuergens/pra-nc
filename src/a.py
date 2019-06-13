92fbedb32b9db1263776b78ded260a13aa21d96b
bjoern <git@wotanii.de>
2019-06-13 11:23:12 +0200

    physic works and also looks very nice

--- src/a.py
+++ src/a.py
@@ -6,12 +6,20 @@ from time import sleep
 physicsClient = p.connect(p.GUI)
 
 p.setGravity(0, 0, -10)
-planeId = p.loadSDF("src/my_room.sdf")
+# planeId = p.loadSDF("src/my_room.sdf")
+# planeId = p.loadSDF("bullet3/examples/pybullet/gym/pybullet_data/table/table.urdf")
+
+p.loadURDF("bullet3/data/plane.urdf")
+#%%
+# r2d2=p.loadURDF("r2d2.urdf",[0,0,0.5])
+
 # planeId = p.loadSDF("src/virtual_room.sdf")
 cubeStartPos = [0, 0, 1]
 cubeStartOrientation = p.getQuaternionFromEuler([0, 0, 0])
-boxId = p.loadSDF("src/model.sdf")
-cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId[0])
+# boxId_orig = p.loadSDF("src/model.sdf")
+boxId = p.loadURDF("bullet3/examples/pybullet/gym/pybullet_data/husky/husky.urdf",[0,0,0.5])
+
+cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId)
 
 useRealTimeSimulation = 0
 
@@ -24,3 +32,4 @@ while 1:
     sleep(0.01)  # Time in seconds.
   else:
     p.stepSimulation()
+    sleep(0.01)  # Time in seconds.
