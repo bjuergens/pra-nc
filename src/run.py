d1d68110a6256071973aa3a6ef7bd464b61e1c55
bjoern <git@wotanii.de>
2019-06-13 18:22:34 +0200

    dummy brain works good and experiment looks very nice

--- src/run.py
+++ src/run.py
@@ -36,19 +36,31 @@ maxForceSlider = p.addUserDebugParameter("maxForce", 0, 150, 100)
 
 camInfo = p.getDebugVisualizerCamera()
 
+
 class my_bain(object):
 
     def __init__(self):
         pass
 
-    def process(self, input):
-        pass
+    def process(self, count_red_left, count_red_right, count_non_red):
+        if count_red_left + count_red_right < count_non_red / 1000:
+            left_speed = -0.8
+            right_speed = 0.8
+        else:
+            if count_red_left > count_red_right:
+                left_speed = 0.8
+                right_speed = 1
+            else:
+                left_speed = 1.0
+                right_speed = 0.8
+
+        return left_speed, right_speed
+
 
 class my_husky(object):
 
-    def __init__(self):
-        self.lastCamTime = time.time()
-        self.lastConTime = time.time()
+    def __init__(self, brain):
+        self.brain = brain
         for joint in range(p.getNumJoints(husky)):
             if 1:
                 print("joint[", joint, "]=", p.getJointInfo(husky, joint))
@@ -63,40 +75,41 @@ class my_husky(object):
             if p.getJointInfo(husky, joint)[1] == b'rear_right_wheel':
                 self.rear_right_wheel = joint
 
-    def update_control(self):
-        now = time.time()
+    def update(self):
+        img = self.update_cam()
+        count_result = self.count_red_pixels(img)
+        command_left, command_right = self.brain.process(*count_result)
+        my_robot.update_control(command_left, command_right)
 
-        if now - self.lastConTime > .1:
-            maxForce = p.readUserDebugParameter(maxForceSlider)
-            targetVelocity = p.readUserDebugParameter(targetVelocitySlider)
-            p.setJointMotorControl2(husky, self.front_left_wheel, p.VELOCITY_CONTROL, targetVelocity=-targetVelocity,
-                                    force=maxForce)
-            p.setJointMotorControl2(husky, self.rear_left_wheel, p.VELOCITY_CONTROL, targetVelocity=-targetVelocity,
-                                    force=maxForce)
-            p.setJointMotorControl2(husky, self.front_right_wheel, p.VELOCITY_CONTROL, targetVelocity=targetVelocity,
-                                    force=maxForce)
-            p.setJointMotorControl2(husky, self.rear_right_wheel, p.VELOCITY_CONTROL, targetVelocity=targetVelocity,
-                                    force=maxForce)
-            self.lastConTime = now
+    def update_control(self, command_left, command_right):
+        maxForce = p.readUserDebugParameter(maxForceSlider)
+        targetVelocity = p.readUserDebugParameter(targetVelocitySlider)
+        p.setJointMotorControl2(husky, self.front_left_wheel, p.VELOCITY_CONTROL,
+                                targetVelocity=command_left * targetVelocity,
+                                force=maxForce)
+        p.setJointMotorControl2(husky, self.rear_left_wheel, p.VELOCITY_CONTROL,
+                                targetVelocity=command_left * targetVelocity,
+                                force=maxForce)
+        p.setJointMotorControl2(husky, self.front_right_wheel, p.VELOCITY_CONTROL,
+                                targetVelocity=command_right * targetVelocity,
+                                force=maxForce)
+        p.setJointMotorControl2(husky, self.rear_right_wheel, p.VELOCITY_CONTROL,
+                                targetVelocity=command_right * targetVelocity,
+                                force=maxForce)
 
     def update_cam(self):
-        # render Camera at 10Hertz
-        now = time.time()
-        if now - self.lastCamTime > .1:
-            ls = p.getLinkState(husky, self.zed_camera_joint, computeForwardKinematics=True)
-            camPos = ls[0]
-            camOrn = ls[1]
-            camMat = p.getMatrixFromQuaternion(camOrn)
-            forwardVec = [camMat[0], camMat[3], camMat[6]]
-            camUpVec = [camMat[2], camMat[5], camMat[8]]
-            camTarget = [camPos[0] + forwardVec[0] * 10, camPos[1] + forwardVec[1] * 10, camPos[2] + forwardVec[2] * 10]
-            viewMat = p.computeViewMatrix(camPos, camTarget, camUpVec)
-            projMat = camInfo[3]
-            # getCameraImage seems to update the debug-view but I don't know why and how
-            img = p.getCameraImage(80, 80, viewMatrix=viewMat, projectionMatrix=projMat,
-                                   renderer=p.ER_BULLET_HARDWARE_OPENGL)
-            print(self.count_red_pixels(img))
-            self.lastCamTime = now
+        ls = p.getLinkState(husky, self.zed_camera_joint, computeForwardKinematics=True)
+        camPos = ls[0]
+        camOrn = ls[1]
+        camMat = p.getMatrixFromQuaternion(camOrn)
+        forwardVec = [camMat[0], camMat[3], camMat[6]]
+        camUpVec = [camMat[2], camMat[5], camMat[8]]
+        camTarget = [camPos[0] + forwardVec[0] * 10, camPos[1] + forwardVec[1] * 10, camPos[2] + forwardVec[2] * 10]
+        viewMat = p.computeViewMatrix(camPos, camTarget, camUpVec)
+        projMat = camInfo[3]
+        # getCameraImage seems to update the debug-view but I don't know why and how
+        return p.getCameraImage(80, 80, viewMatrix=viewMat, projectionMatrix=projMat,
+                                renderer=p.ER_BULLET_HARDWARE_OPENGL)
 
     def count_red_pixels(self, img_arr):
         """detect left red and right red like nrp
@@ -118,23 +131,34 @@ class my_husky(object):
                     else:
                         count_red_left = count_red_left + 1
                 else:
-                    count_non_red = count_non_red + count_non_red
+                    count_non_red = count_non_red + 1
         return count_red_left, count_red_right, count_non_red
 
 
-my_robot = my_husky()
-count = 0
+my_robot = my_husky(brain=my_bain())
+
+count_step = 0
+last_robot_update_time = time.time()
+count_switch = 0
+last_switch_time = 0
 while 1:
-    count = count + 1
-    if count % 10 == 0:
-        if count % 2 == 0:
-            p.changeVisualShape(planeA, -1, rgbaColor=red)
-            p.changeVisualShape(planeB, -1, rgbaColor=green)
-        else:
+    count_step = count_step + 1
+    now = time.time()
+
+    if now - last_switch_time > 5:
+        last_switch_time = now
+        count_switch = count_switch + 1
+        print(count_switch)
+        if count_switch % 2 == 0:
             p.changeVisualShape(planeA, -1, rgbaColor=green)
             p.changeVisualShape(planeB, -1, rgbaColor=red)
+        else:
+            p.changeVisualShape(planeA, -1, rgbaColor=red)
+            p.changeVisualShape(planeB, -1, rgbaColor=green)
 
-    my_robot.update_cam()
-    my_robot.update_control()
+    # render Camera at 10Hertz
+    if now - last_robot_update_time > .1:
+        my_robot.update()
+        last_robot_update_time = now
     p.stepSimulation()
-    # time.sleep(0.0005)
+    time.sleep(0.0005)
