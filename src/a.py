a613e86186302a27aaaf54eec0abbb442289ef56
bjoern <git@wotanii.de>
2019-06-13 15:03:13 +0200

    robot can drive

--- src/a.py
+++ src/a.py
@@ -1,8 +1,9 @@
  #!/usr/bin/env python3
 
 import pybullet as p
-from time import sleep
+import time
 import pybullet_data
+
 
 physicsClient = p.connect(p.GUI) # p.DIRECT for non-graphical version
 
@@ -12,9 +13,9 @@ p.loadURDF("plane.urdf")
 cubeStartPos = [0, 0, 1]
 cubeStartOrientation = p.getQuaternionFromEuler([0, 0, 0])
 
-boxId = p.loadURDF("husky/husky.urdf",cubeStartPos, cubeStartOrientation)
+husky = p.loadURDF("husky/husky.urdf",cubeStartPos, cubeStartOrientation)
 
-cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId)
+cubePos, cubeOrn = p.getBasePositionAndOrientation(husky)
 
 planeId = p.createCollisionShape(p.GEOM_BOX, halfExtents=[1,0.25,1])
 planeVisId = p.createVisualShape(p.GEOM_BOX, halfExtents=[1,0.25,1],  rgbaColor=[1, 1, 0, 1])
@@ -29,7 +30,65 @@ p.changeVisualShape(planeB, -1, rgbaColor=[0, 0, 1, 1])
 
 p.setRealTimeSimulation(0)
 
+
+targetVelocitySlider = p.addUserDebugParameter("wheelVelocity",-5,5,0)
+maxForceSlider = p.addUserDebugParameter("maxForce",0,5,2)
+
+camInfo = p.getDebugVisualizerCamera()
+
+class my_husky(object):
+
+    def __init__(self):
+        self.lastCamTime = time.time()
+        self.lastConTime = time.time()
+        for joint in range(p.getNumJoints(husky)):
+            if 1:
+                print("joint[",joint,"]=", p.getJointInfo(husky,joint))
+            if p.getJointInfo(husky,joint)[1]==b'user_rail':
+                self.zed_camera_joint = joint
+            if p.getJointInfo(husky,joint)[1]==b'front_left_wheel':
+                self.front_left_wheel = joint
+            if p.getJointInfo(husky,joint)[1]==b'front_right_wheel':
+                self.front_right_wheel = joint
+            if p.getJointInfo(husky,joint)[1]==b'rear_left_wheel':
+                self.rear_left_wheel = joint
+            if p.getJointInfo(husky,joint)[1]==b'rear_right_wheel':
+                self.rear_right_wheel = joint
+
+    def update_control(self):
+        nowTime = time.time()
+
+        if (nowTime - self.lastConTime > .1):
+            maxForce = p.readUserDebugParameter(maxForceSlider)
+            targetVelocity = p.readUserDebugParameter(targetVelocitySlider)
+            p.setJointMotorControl2(husky,self.front_left_wheel,p.VELOCITY_CONTROL,targetVelocity=targetVelocity,force=maxForce)
+            p.setJointMotorControl2(husky,self.front_right_wheel,p.VELOCITY_CONTROL,targetVelocity=targetVelocity,force=maxForce)
+            p.setJointMotorControl2(husky,self.rear_left_wheel,p.VELOCITY_CONTROL,targetVelocity=targetVelocity,force=maxForce)
+            p.setJointMotorControl2(husky,self.rear_right_wheel,p.VELOCITY_CONTROL,targetVelocity=targetVelocity,force=maxForce)
+            self.lastConTime = nowTime
+
+    def update_cam(self):
+        # render Camera at 10Hertz
+        nowTime = time.time()
+        if (nowTime - self.lastCamTime > .1):
+            ls = p.getLinkState(husky, self.zed_camera_joint, computeForwardKinematics=True)
+            camPos = ls[0]
+            camOrn = ls[1]
+            camMat = p.getMatrixFromQuaternion(camOrn)
+            upVector = [0, 0, 1]
+            forwardVec = [camMat[0], camMat[3], camMat[6]]
+            # sideVec =  [camMat[1],camMat[4],camMat[7]]
+            camUpVec = [camMat[2], camMat[5], camMat[8]]
+            camTarget = [camPos[0] + forwardVec[0] * 10, camPos[1] + forwardVec[1] * 10, camPos[2] + forwardVec[2] * 10]
+            camUpTarget = [camPos[0] + camUpVec[0], camPos[1] + camUpVec[1], camPos[2] + camUpVec[2]]
+            viewMat = p.computeViewMatrix(camPos, camTarget, camUpVec)
+            projMat = camInfo[3]
+            # p.getCameraImage(320,200,viewMatrix=viewMat,projectionMatrix=projMat, flags=p.ER_NO_SEGMENTATION_MASK, renderer=p.ER_BULLET_HARDWARE_OPENGL)
+            p.getCameraImage(320, 200, viewMatrix=viewMat, projectionMatrix=projMat, renderer=p.ER_BULLET_HARDWARE_OPENGL)
+            self.lastCamTime = nowTime
 count=0
+
+my_robot = my_husky()
 
 while 1:
     count = count +1
@@ -40,5 +99,9 @@ while 1:
         else:
             p.changeVisualShape(planeA, -1, rgbaColor=green)
             p.changeVisualShape(planeB, -1, rgbaColor=blue)
+
+    my_robot.update_cam()
+    my_robot.update_control()
     p.stepSimulation()
-    sleep(0.005)
+    time.sleep(0.005)
+
